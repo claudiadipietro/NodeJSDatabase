9d6799aa148efdafcb0b4f158fd0b359f8e2234c
Claudia Di Pietro <claudiadipietro12@gmail.com>
2022-06-29 23:20:21 +0200

    Api keyword added with routes. Body parser library implemented

--- api-rest-node/index.js
+++ api-rest-node/index.js
@@ -1,10 +1,15 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3500;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/forum', { useNewUrlParser: true})
         .then(() => {
             console.log('The connection to the Database is correctly done');
+            app.listen(port, () => {
+                console.log("The server is working")
+            })
         })
         .catch(error => console.log(error));
